c82e8b91444cdcd89067c3f58699f64f161e24df
“wonkdance” <wonkdance@gmail.com>
2021-06-06 14:25:26 +0900

    mjwt.ValidateAccessToken test

--- pkg/models/mjwt/jwttoken_test.go
+++ pkg/models/mjwt/jwttoken_test.go
@@ -26,3 +26,16 @@ func TestGenerateAccessToken(t *testing.T) {
 	expected := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDEwMzg1ODMsInJldHVybl91cmkiOiJodHRwOi8vbG9jYWwuZXhhbXBsZS5jb20vcmV0dXJuP25hbWU9d29ua1x1MDAyNmlkPXdvbmsiLCJ1c3JfaWQiOiJ3b25rIn0.Mxm4Hq60W2iL5QT2ORj8UMA40fsbiXFTw-IuCoypPuA"
 	assert.Equal(t, expected, token)
 }
+
+func TestValidateAccessToken(t *testing.T) {
+	tokenSecret := "asdfasdf1234"
+	userID := "wonk"
+	returnURI := "http://local.example.com/return?name=wonk&id=wonk"
+	accessToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDEwMzg1ODMsInJldHVybl91cmkiOiJodHRwOi8vbG9jYWwuZXhhbXBsZS5jb20vcmV0dXJuP25hbWU9d29ua1x1MDAyNmlkPXdvbmsiLCJ1c3JfaWQiOiJ3b25rIn0.Mxm4Hq60W2iL5QT2ORj8UMA40fsbiXFTw-IuCoypPuA"
+	claim, _, err := mjwt.ValidateAccessToken(accessToken, tokenSecret)
+	if !assert.Nil(t, err) {
+		t.FailNow()
+	}
+	assert.Equal(t, userID, claim.UsrID)
+	assert.Equal(t, returnURI, claim.ReturnURI)
+}
